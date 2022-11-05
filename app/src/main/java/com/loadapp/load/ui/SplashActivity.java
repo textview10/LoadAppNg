3b90d509195649562834633c1eeb0ab5a675ddd9
wangxu <867103701@qq.com>
2022-11-05 21:55:44 +0800

    [feature]:增加跳转链接.

--- app/src/main/java/com/loadapp/load/ui/SplashActivity.java
+++ app/src/main/java/com/loadapp/load/ui/SplashActivity.java
@@ -3,28 +3,50 @@ package com.loadapp.load.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
 import com.loadapp.load.R;
 import com.loadapp.load.base.BaseActivity;
 import com.loadapp.load.ui.home.HomeActivity;
+import com.loadapp.load.ui.login.SignInActivity;
+import com.loadapp.load.ui.login.SignUpActivity;
 
 /**
  * splash 
  */
 public class SplashActivity extends BaseActivity {
 
+    private RelativeLayout rlContainer;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_spalish);
+        setContentView(R.layout.activity_welcome);
+        rlContainer = findViewById(R.id.rl_welcome_container);
+        findViewById(R.id.rl_welcome_signin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, SignInActivity.class));
+            }
+        });
+        findViewById(R.id.rl_welcome_signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SplashActivity.this, SignUpActivity.class));
+            }
+        });
 
-        findViewById(R.id.btn_test_to_main).setOnClickListener(new View.OnClickListener() {
+        Button btn = new Button(SplashActivity.this);
+        btn.setText("测试进入首页");
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SplashActivity.this, HomeActivity.class));
             }
         });
+        rlContainer.addView(btn);
     }
 }
--- app/src/main/java/com/loadapp/load/ui/login/SignInActivity.java
+++ app/src/main/java/com/loadapp/load/ui/login/SignInActivity.java
@@ -1,6 +1,77 @@
 package com.loadapp.load.ui.login;
 
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.loadapp.load.R;
+import com.loadapp.load.api.Api;
 import com.loadapp.load.base.BaseActivity;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignInActivity extends BaseActivity {
+    private static final String TAG = "SignInActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+        testLogin();
+//        testCheckPhoneNum();
+    }
+
+    private void testCheckPhoneNum() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("mobile", System.currentTimeMillis());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        OkGo.<String>post(Api.CHECK_MOBILE).tag(TAG)
+                .params("mobile", System.currentTimeMillis())
+                .upJson(jsonObject).execute(new StringCallback() {
+            @Override
+            public void onSuccess(Response<String> response) {
+                Log.e(TAG, "response 1 = " + response.body().toString());
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                super.onError(response);
+            }
+        });
+    }
+
+    private void testLogin() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+//            JSONObject data = new JSONObject();
+//            jsonObject.put("data", data);
+            jsonObject.put("request_time", System.currentTimeMillis());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.e(TAG, " = " + jsonObject.toString());
+        OkGo.<String>post(Api.CHECK_SERVER_ALIVE).tag(TAG)
+                .params("data",jsonObject.toString())
+                .execute(new StringCallback() {
+            @Override
+            public void onSuccess(Response<String> response) {
+                Log.e(TAG, "response 2 = " + response.body().toString());
+            }
+
+            @Override
+            public void onError(Response<String> response) {
+                super.onError(response);
+            }
+        });
+
+    }
 }
