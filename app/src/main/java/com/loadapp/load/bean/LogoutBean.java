1af90e7e77d6d2b31d9d6de6396582a710a16456
wangxu <867103701@qq.com>
2022-11-19 16:58:59 +0800

    [bug]:我的界面

---
+++ app/src/main/java/com/loadapp/load/bean/LogoutBean.java
@@ -0,0 +1,13 @@
+package com.loadapp.load.bean;
+
+public class LogoutBean {
+    private String server_time;
+
+    public String getServer_time() {
+        return server_time;
+    }
+
+    public void setServer_time(String server_time) {
+        this.server_time = server_time;
+    }
+}
--- app/src/main/java/com/loadapp/load/ui/home/MeFragment.java
+++ app/src/main/java/com/loadapp/load/ui/home/MeFragment.java
@@ -1,27 +1,125 @@
 package com.loadapp.load.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.loadapp.load.R;
+import com.loadapp.load.api.Api;
 import com.loadapp.load.base.BaseFragment;
+import com.loadapp.load.bean.AccountProfileBean;
+import com.loadapp.load.bean.LogoutBean;
+import com.loadapp.load.global.Constant;
+import com.loadapp.load.ui.SplashActivity;
+import com.loadapp.load.util.BuildRequestJsonUtil;
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.model.Response;
 
-public class MeFragment extends BaseFragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MeFragment extends BaseFragment implements View.OnClickListener {
+
+    private static final String TAG = "MeFragment";
+
+    private LinearLayout llInfo, llBankCard, llMsg, llHelp;
+    private FrameLayout flLogout;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_me, container,false);
+        return inflater.inflate(R.layout.fragment_me, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-//        view.findViewById()
+        llInfo = view.findViewById(R.id.ll_me_infomation);
+        llBankCard = view.findViewById(R.id.ll_me_bank_card);
+        llMsg = view.findViewById(R.id.ll_me_message);
+        llHelp = view.findViewById(R.id.ll_me_help);
+        flLogout = view.findViewById(R.id.fl_me_logout);
+
+        llInfo.setOnClickListener(this);
+        llBankCard.setOnClickListener(this);
+        llMsg.setOnClickListener(this);
+        llHelp.setOnClickListener(this);
+        flLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_me_infomation:
+
+                break;
+            case R.id.ll_me_bank_card:
+
+                break;
+            case R.id.ll_me_message:
+
+                break;
+            case R.id.ll_me_help:
+
+                break;
+            case R.id.fl_me_logout:
+                logOut();
+                break;
+        }
+    }
+
+    private void logOut() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("account_id", Constant.mAccountId + "");
+            jsonObject.put("request_time", System.currentTimeMillis());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        OkGo.<String>post(Api.LOGOUT).tag(TAG)
+                .params("data", jsonObject.toString())
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        if (getActivity().isFinishing() || getActivity().isDestroyed()) {
+                            return;
+                        }
+                        LogoutBean logoutBean = checkResponseSuccess(response, LogoutBean.class);
+                        if (logoutBean == null) {
+                            Log.e(TAG, " logout error ." + response.body());
+                            return;
+                        }
+                        Intent intent = new Intent(getContext(), SplashActivity.class);
+                        startActivity(intent);
+                        getActivity().finish();
+                        ToastUtils.showShort("logout success");
+                    }
+
+                    @Override
+                    public void onError(Response<String> response) {
+                        super.onError(response);
+                        if (getActivity().isFinishing() || getActivity().isDestroyed()) {
+                            return;
+                        }
+                        Log.e(TAG, "logout failure = " + response.body());
+                        ToastUtils.showShort("logout failure");
+                    }
+                });
+    }
+
+    @Override
+    public void onDestroyView() {
+        OkGo.getInstance().cancelTag(TAG);
+        super.onDestroyView();
     }
 }
