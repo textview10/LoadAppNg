af3274cf6d31b075a45db863ee204be8ca448303
wangxu <867103701@qq.com>
2022-11-20 16:20:35 +0800

    [feature]:添加首页界面.

---
+++ app/src/main/java/com/loadapp/load/ui/home/status/BaseStatusFragment.java
@@ -0,0 +1,8 @@
+package com.loadapp.load.ui.home.status;
+
+import com.loadapp.load.base.BaseFragment;
+
+public class BaseStatusFragment extends BaseFragment {
+
+
+}
---
+++ app/src/main/java/com/loadapp/load/ui/home/status/LoanProcessingFragment.java
@@ -0,0 +1,29 @@
+package com.loadapp.load.ui.home.status;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.loadapp.load.R;
+
+/**
+ * 借贷处理中...
+ */
+public class LoanProcessingFragment extends BaseStatusFragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_loan_proceeing, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
