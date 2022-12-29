602f8a099b4c555eff0c586b9907ff8ed449d52c
wang xu <wang867103701@gmail.com>
2022-12-29 23:23:12 +0800

    [feature]:注册前需要同意协议

--- app/src/main/java/com/loadapp/load/ui/login/SignUpActivity.java
+++ app/src/main/java/com/loadapp/load/ui/login/SignUpActivity.java
@@ -3,8 +3,12 @@ package com.loadapp.load.ui.login;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.blankj.utilcode.util.BarUtils;
 import com.loadapp.load.R;
@@ -18,6 +22,7 @@ import com.loadapp.load.ui.login.fragment.InputPhoneNumFragment;
 import com.loadapp.load.ui.login.fragment.SetPwdFragment;
 import com.loadapp.load.ui.login.fragment.VerifySmsCodeFragment;
 import com.loadapp.load.ui.webview.AgreeTermFragment;
+import com.loadapp.load.ui.webview.WebViewFragment;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
 import com.lzy.okgo.model.Response;
@@ -27,13 +32,15 @@ import org.json.JSONObject;
 
 public class SignUpActivity extends BaseActivity {
     private static final String TAG = "SignUpActivity";
-
+    private WebViewFragment webViewFragment;
+    private FrameLayout flWebView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         BarUtils.setStatusBarVisibility(this, false);
         setContentView(R.layout.activity_sign_up);
+        flWebView = findViewById(R.id.fl_sign_up_container2);
 //        verifySmsCode("18518511461", "6666");
 //        register("18518511461","aa123456");
         AgreeTermFragment agreeTermFragment = new AgreeTermFragment();
@@ -45,30 +52,48 @@ public class SignUpActivity extends BaseActivity {
         return R.id.fl_sign_up_container;
     }
 
-    public void toInputPhoneNum(){
+    public void toInputPhoneNum() {
         InputPhoneNumFragment inputPhoneNumFragment = new InputPhoneNumFragment();
         toFragment(inputPhoneNumFragment);
     }
 
-    public void toVerifySmsCodePage(String phoneNum){
+    public void toVerifySmsCodePage(String phoneNum) {
         VerifySmsCodeFragment verifySmsCodeFragment = new VerifySmsCodeFragment();
         verifySmsCodeFragment.setPhoneNum(phoneNum);
         toFragment(verifySmsCodeFragment);
     }
 
-    public void toSetPwdPage(String phoneNum){
+    public void toSetPwdPage(String phoneNum) {
         SetPwdFragment setPwdFragment = new SetPwdFragment();
         setPwdFragment.setPhoneNum(phoneNum);
         toFragment(setPwdFragment);
     }
 
-    public void toHomePage(){
+    public void toHomePage() {
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         finish();
     }
 
-    public void backPress(){
+    public void toWebView() {
+        flWebView.setVisibility(View.VISIBLE);
+        if (webViewFragment == null) {
+            webViewFragment = new WebViewFragment();
+        }
+        webViewFragment.setUrl(Api.WEB_VIEW_POLICY);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();   // 开启一个事务
+        transaction.replace(R.id.fl_sign_up_container2, webViewFragment);
+        transaction.commitAllowingStateLoss();
+    }
+
+    public void backPress() {
+        if (flWebView != null) {
+            if (flWebView.getVisibility() == View.VISIBLE) {
+                flWebView.setVisibility(View.GONE);
+                return;
+            }
+        }
         Intent intent = new Intent(this, SplashActivity.class);
         startActivity(intent);
         finish();
--- app/src/main/java/com/loadapp/load/ui/login/fragment/InputPhoneNumFragment.java
+++ app/src/main/java/com/loadapp/load/ui/login/fragment/InputPhoneNumFragment.java
@@ -7,8 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,9 +19,11 @@ import androidx.appcompat.widget.AppCompatEditText;
 import com.blankj.utilcode.util.ToastUtils;
 import com.loadapp.load.R;
 import com.loadapp.load.api.Api;
+import com.loadapp.load.base.BaseActivity;
 import com.loadapp.load.base.BaseFragment;
 import com.loadapp.load.presenter.PhoneNumPresenter;
 import com.loadapp.load.ui.login.SignUpActivity;
+import com.loadapp.load.ui.webview.WebViewFragment;
 import com.loadapp.load.util.BuildRequestJsonUtil;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.callback.StringCallback;
@@ -34,6 +38,9 @@ public class InputPhoneNumFragment extends BaseFragment {
     private AppCompatEditText etInput;
     private PhoneNumPresenter presenter;
     private Spinner spinner;
+    private WebViewFragment webViewFragment;
+    private ImageView checkAgree;
+    private boolean isAgree = false;
 
     @Nullable
     @Override
@@ -49,12 +56,19 @@ public class InputPhoneNumFragment extends BaseFragment {
         etInput = view.findViewById(R.id.et_signup_input);
         RelativeLayout rlCommit = view.findViewById(R.id.rl_signup_input_commit);
 
+        checkAgree = view.findViewById(R.id.iv_signup_agree);
+        TextView tvTerm = view.findViewById(R.id.tv_signup_term);
+
         presenter = new PhoneNumPresenter(getContext());
         presenter.initSpinner(spinner);
 
         rlCommit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (!isAgree){
+                    ToastUtils.showShort(" must agree terms");
+                    return;
+                }
                 boolean isNotAvailable = checkPhoneNumLocal(etInput);
                 if (isNotAvailable) {
                     ToastUtils.showShort("phone num is not input.");
@@ -65,6 +79,20 @@ public class InputPhoneNumFragment extends BaseFragment {
             }
         });
 
+        checkAgree.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isAgree = !isAgree;
+                updateAgreeState();
+            }
+        });
+        tvTerm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SignUpActivity activity = (SignUpActivity) getActivity();
+                activity.toWebView();
+            }
+        });
     }
 
     private boolean checkPhoneNumLocal(AppCompatEditText et) {
@@ -139,5 +167,11 @@ public class InputPhoneNumFragment extends BaseFragment {
                         Log.e(TAG, "send sms request error = " + response.body());
                     }
                 });
+    }
+
+    private void updateAgreeState(){
+        if (checkAgree != null){
+            checkAgree.setImageResource(isAgree ? R.drawable.ic_selected : R.drawable.ic_unselected);
+        }
     }
 }
